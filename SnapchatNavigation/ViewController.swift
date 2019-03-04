1d61b269e8f7b64883dfd9fbe10d3f15481edcc7
[S] Artur Chabera <artur.chabera@snow.dog>
2019-03-04 11:37:49 +0100

    Initial Commit

---
+++ SnapchatNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapchatNavigation
+//
+//  Created by Artur Chabera on 04/03/2019.
+//  Copyright © 2019 Artur Chabera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
